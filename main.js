b762a22ebc0490806517ad53813d5e3288ec855d
limesnej <jensskjetne28@gmail.com>
2019-10-18 09:06:03 +0200

    socket.io fixing stuff from starting point

--- main.js
+++ main.js
@@ -1,44 +1,39 @@
 const express = require('express');
 const fs = require('fs');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http); // bind socket io to our server
+const path = require('path')
+// const http = require('http').createServer(app);
+const socketIO = require('socket.io') // bind socket io to our server
 const port1 = process.env.PORT || 3000;
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 const port = new SerialPort ('COM4', {baudRate: 9600});
 const parser = port.pipe(new Readline({delimiter: '\n'}));
+const INDEX = path.join(__dirname, '/public');
 
+const server = express ()
+    .use((req, res) => res.sendFile(INDEX))
+    .listen(port1, () => console.log(`Listening on on ${port1}`));
 
-app.use(express.static('public')); // send index.html page on GET /
+const io = socketIO(server);
 
-io.on('connection', function(socket){ // listening for the connection event and log it
-    console.log('a user connected');
-
-    parser.on('data', function(data) {
-        let str = data;
-    
-        let separ = str.split(", ");
-        let CO = separ[0];
-        let temp = separ[1];
-        let RH = separ[2];
-        const today = new Date();
-        
-        fs.appendFile('sensordata.txt', `\n${CO}, ${temp}, ${RH}, ${today.getDate()+"-"+today.getMonth()+1+"-"+today.getFullYear()+" "+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds()}`, (err) =>{
-            if (err) return console.log(err);
-            console.log('The data was appended to file!');
-        });
-        
-        console.log(CO, temp, RH);
-        
-        io.sockets.emit('thedata', {date: today.getDate()+"-"+today.getMonth() + 1 +"-"+today.getFullYear(), time: (today.getHours())+':'+(today.getMinutes())+':'+(today.getSeconds()),co:CO, temperature:temp, moist:RH});
-    });
-
+io.on('connection', (socket) => {
+    console.log('Client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-http.listen(port1, function() {
-    console.log(`listening on ${port1}`);
-});
+
+
+
+// app.use(express.static(__dirname + '/public')); // send index.html page on GET /
+
+// io.on('connection', function(socket){ // listening for the connection event and log it
+//     console.log('a user connected');
+
+// });
+
+// http.listen(port1, function() {
+//     console.log(`listening on ${port1}`);
+// });
 
 // const SerialPort = require('serialport');
 // const Readline = require('@serialport/parser-readline');
@@ -48,24 +43,24 @@ http.listen(port1, function() {
 
 
 
-
-
-// parser.on('data', function(data) {
-//     let str = data;
-
-//     let separ = str.split(", ");
-//     let CO = separ[0];
-//     let temp = separ[1];
-//     let RH = separ[2];
-//     const today = new Date();
+parser.on('data', function(data) {
     
-//     fs.appendFile('sensordata.txt', `\n${CO}, ${temp}, ${RH}, ${today.getDate()+"-"+today.getMonth()+1+"-"+today.getFullYear()+" "+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds()}`, (err) =>{
-//         if (err) return console.log(err);
-//         console.log('The data was appended to file!');
-//     });
     
-//     console.log(CO, temp, RH);
+    let str = data;
+
+    let separ = str.split(", ");
+    let CO = separ[0];
+    let temp = separ[1];
+    let RH = separ[2];
+    const today = new Date();
     
-//     io.socket.emit('thedata', {date: today.getDate()+"-"+today.getMonth() + 1 +"-"+today.getFullYear(), time: (today.getHours())+':'+(today.getMinutes())+':'+(today.getSeconds()),co:CO, temperature:temp, moist:RH});
-// });
+    fs.appendFile('sensordata.txt', `\n${CO}, ${temp}, ${RH}, ${today.getDate()+"-"+today.getMonth()+1+"-"+today.getFullYear()+" "+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds()}`, (err) =>{
+        if (err) return console.log(err);
+        console.log('The data was appended to file!');
+    });
+    
+    console.log(CO, temp, RH);
+    
+    io.emit('thedata', {date: today.getDate()+"-"+today.getMonth() + 1 +"-"+today.getFullYear(), time: (today.getHours())+':'+(today.getMinutes())+':'+(today.getSeconds()),co:CO, temperature:temp, moist:RH});
+});
 
