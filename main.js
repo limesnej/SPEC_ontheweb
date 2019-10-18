3c4bfef8bb9a730c10f253d52df20a1d54fbd21d
limesnej <jensskjetne28@gmail.com>
2019-10-18 09:53:12 +0200

    tried something else in html

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
 const express = require('express');
+const app = express();
 const fs = require('fs');
 const path = require('path')
-// const http = require('http').createServer(app);
-const socketIO = require('socket.io') // bind socket io to our server
+const http = require('http').createServer(app);
+const socketIO = require('socket.io') 
+const io = require('socket.io')(http);
 const port1 = process.env.PORT || 3000;
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 const port = new SerialPort ('COM4', {baudRate: 9600});
 const parser = port.pipe(new Readline({delimiter: '\n'}));
-const INDEX = path.join(__dirname, '/public/index.html');
 
-const server = express ()
-    .use((req, res) => res.sendFile(INDEX))
-    .listen(port1, () => console.log(`Listening on on ${port1}`));
 
-const io = socketIO(server);
+// const server = express ()
+//     .use((req, res) => res.sendFile(INDEX))
+//     .listen(port1, () => console.log(`Listening on on ${port1}`));
 
-io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
+// const io = socketIO(server);
+
+// io.on('connection', (socket) => {
+//     console.log('Client connected');
+//     socket.on('disconnect', () => console.log('Client disconnected'));
+// });
+
+
+
+
+app.use(express.static(__dirname + '/public')); // send index.html page on GET /
+
+io.on('connection', function(socket){ // listening for the connection event and log it
+    console.log('a user connected');
+
 });
 
-
-
-
-// app.use(express.static(__dirname + '/public')); // send index.html page on GET /
-
-// io.on('connection', function(socket){ // listening for the connection event and log it
-//     console.log('a user connected');
-
-// });
-
-// http.listen(port1, function() {
-//     console.log(`listening on ${port1}`);
-// });
+http.listen(port1, function() {
+    console.log(`listening on ${port1}`);
+});
 
 // const SerialPort = require('serialport');
 // const Readline = require('@serialport/parser-readline');
