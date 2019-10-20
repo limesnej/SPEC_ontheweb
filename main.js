e66992cd897f79c1ab03d4102263b46f2c97a2d5
limesnej <jensskjetne28@gmail.com>
2019-10-20 20:45:57 +0200

    made it better!

--- main.js
+++ main.js
@@ -4,10 +4,15 @@ const fs = require('fs');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const port1 = process.env.PORT || 3000;
-const SerialPort = require('serialport');
-const Readline = require('@serialport/parser-readline');
-const port = new SerialPort ('COM4', {baudRate: 9600});
-const parser = port.pipe(new Readline({delimiter: '\n'}));
+// const SerialPort = require('serialport');
+// const Readline = require('@serialport/parser-readline');
+// const port = new SerialPort ('COM4', {baudRate: 9600});
+// const parser = port.pipe(new Readline({delimiter: '\n'}));
+const dweetClient = require('node-dweetio');
+const moment = require('moment');
+
+dweetio = new dweetClient();
+const dweetThing = 'spec-co';
 
 
 
@@ -19,22 +24,14 @@ io.on('connection', function(socket){ // listening for the connection event and 
 
 });
 
-parser.on('data', function(data) {
-    let separ = data.split(", ");
-    let CO = separ[0];
-    let temp = separ[1];
-    let RH = separ[2];
-    const today = new Date();
-    
-    fs.appendFile('sensordata.txt', `\n${CO}, ${temp}, ${RH}, ${today.getDate()+"-"+today.getMonth()+1+"-"+today.getFullYear()+" "+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds()}`, (err) =>{
-        if (err) return console.log(err);
-        console.log('The data was appended to file!');
-    });
-    
-    console.log(CO, temp, RH);
-    
-    io.emit('thedata', {date: today.getDate()+"-"+today.getMonth() + 1 +"-"+today.getFullYear(), time: (today.getHours())+':'+(today.getMinutes())+':'+(today.getSeconds()),co:CO, temperature:temp, moist:RH});
-});
+dweetio.listen_for(dweetThing, (dweet)=> {
+    const data = {
+        sensorData: dweet.content,
+        time: moment().format('HH:mm:ss')
+    };
+    io.emit('sensor-data', data);
+})
+
 
 
 app.use(express.static(__dirname + '/public')); // send index.html page on GET /
