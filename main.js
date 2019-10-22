1a5f22e49a824c235419cf1ad9813f5a1aae7dfa
limesnej <jensskjetne28@gmail.com>
2019-10-22 14:17:48 +0200

    make it better

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(bodyParser.json());
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 // app.get('/', (req, res) => {
 //     res.sendFile(__dirname + 'index.html');
 // });
