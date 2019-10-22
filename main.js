41465b861f387ea32fd31f827dc5bd03d1dbd591
limesnej <jensskjetne28@gmail.com>
2019-10-22 14:11:26 +0200

    make it work

--- main.js
+++ main.js
@@ -23,9 +23,9 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 // app.use(express.static(__dirname + '/public'));
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'index.html');
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + 'index.html');
+// });
 
 app.use((req, res)=> {
     res.status(404).json({
